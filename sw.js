34f32217d0b2b951e7aca2b7ee1f677639709508
jesseemry <jesse.emry@acsgmail.net>
2025-01-15 09:51:47 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('signageApp')
+        .then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/javascript.js',
+                '/Stylesheet.css'
+        });
+    );
+});
+
+self.addEventListener('online', () => {
+    console.log('online');
+    clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            client.postMessage('refresh');
+        });
+    });
+});
