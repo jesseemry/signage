6240139b04036fb86fba667a1c40f9fde536a681
jesseemry <jesse.emry@acsgmail.net>
2025-01-15 12:35:52 -0500

    Update sw.js
    trying to just loadcontent immediately instead of sending a message

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const swVersion = '1.0';
+console.log('sw v#',swVersion);
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('signageApp')
@@ -11,31 +13,32 @@ self.addEventListener('install', (event) => {
     );
 });
 
-(async () => {
-    importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-window.prod.v6.5.4.js');
+// (async () => {
+//     importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-window.prod.v6.5.4.js');
     
-    const registration = await workbox.core.clientsClaim();
-    const clientsClaim = await registration.waitUntil(
-        self.clients.claim()
-    );
-    self.addEventListener('online', () => {
-        console.log('online')
-        clients.matchAll().then(clients => {
-            clients.forEach(client => {
-                client.postMessage('refresh');
-            });
-        });
-    });
-    })();
-
-// self.addEventListener('online', () => {
-//     console.log('online')
-//     clients.matchAll().then(clients => {
-//         clients.forEach(client => {
-//             client.postMessage('refresh');
+//     const registration = await workbox.core.clientsClaim();
+//     const clientsClaim = await registration.waitUntil(
+//         self.clients.claim()
+//     );
+//     self.addEventListener('online', () => {
+//         console.log('online')
+//         clients.matchAll().then(clients => {
+//             clients.forEach(client => {
+//                 client.postMessage('refresh');
+//             });
 //         });
 //     });
-// });
+//     })();
+
+self.addEventListener('online', () => {
+    console.log('online')
+    loadContent()
+    // clients.matchAll().then(clients => {
+    //     clients.forEach(client => {
+    //         client.postMessage('refresh');
+    //     });
+    // });
+});
 
 // self.addEventListener('offline', () => {
 //     console.log('offline')
